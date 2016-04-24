1b51fb8f7646f34e94230db68f1914bb8fc9a3dc
Lyrael Rayne <darkangael@gmail.com>
2016-04-24 20:48:54 +1000

    Changelog and version bump.

--- src/main/java/au/lyrael/stacywolves/StacyWolves.java
+++ src/main/java/au/lyrael/stacywolves/StacyWolves.java
@@ -27,7 +27,7 @@ public class StacyWolves {
     public static final String MOD_ID = "stacywolves";
     public static final String MOD_NAME = "Stacy's Wolves Mod";
     public static final String MC_VERSION = "1.7.10";
-    public static final String VERSION  = "1.0-" + MC_VERSION;
+    public static final String VERSION  = "1.0.1-" + MC_VERSION;
     public static final String ACCEPTED_MC_VERSIONS = "[" + MC_VERSION + "]";
     public static final String DESC = "Cool Story Bro..... but it Needs Moar Wolves!";
     public static final String URL = ""; // PUT CURSEFORGE PROJECT URL HERE!
