bb915fa1ddc89ddced793bc2be4a0a656d73ed1b
Lyrael Rayne <darkangael@gmail.com>
2016-04-25 18:47:51 +1000

    Specifically blacklisted "Mesa Plateau F M" from birch wolf as it's not properly dictionary tagged as a Mesa biome.

--- src/main/java/au/lyrael/stacywolves/entity/wolf/EntityBirchWolf.java
+++ src/main/java/au/lyrael/stacywolves/entity/wolf/EntityBirchWolf.java
@@ -14,7 +14,7 @@ import static net.minecraftforge.common.BiomeDictionary.Type.*;
 
 @WolfMetadata(name = "EntityBirchWolf", primaryColour = 0x2F332C, secondaryColour = 0xEEEEE9,
         spawns = {@WolfSpawn(spawnBiomes = {
-                @WolfSpawnBiome(requireBiomeTypes = {FOREST}, excludeBiomeTypes = {CONIFEROUS, DENSE, JUNGLE, MOUNTAIN, MESA, END, NETHER}),
+                @WolfSpawnBiome(requireBiomeTypes = {FOREST}, excludeBiomeTypes = {CONIFEROUS, DENSE, JUNGLE, MOUNTAIN, MESA, END, NETHER, MESA}, excludeBiomeNames = {"Mesa Plateau F M"}),
         }, probability = 10, min = 1, max = 4),
         })
 public class EntityBirchWolf extends EntityWolfBase implements IRenderableWolf {
