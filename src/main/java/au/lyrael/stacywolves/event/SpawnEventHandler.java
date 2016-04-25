64953e43d91e9dcd20dc7798633878659dfd1cdb
Lyrael Rayne <darkangael@gmail.com>
2016-04-25 11:04:34 +1000

    Better logging for spawn events.

--- src/main/java/au/lyrael/stacywolves/event/SpawnEventHandler.java
+++ src/main/java/au/lyrael/stacywolves/event/SpawnEventHandler.java
@@ -3,13 +3,12 @@ package au.lyrael.stacywolves.event;
 import au.lyrael.stacywolves.entity.ISpawnable;
 import au.lyrael.stacywolves.entity.wolf.IWolf;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static au.lyrael.stacywolves.StacyWolves.MOD_ID;
-import static au.lyrael.stacywolves.StacyWolves.WOLF_REGISTRY;
 import static cpw.mods.fml.common.eventhandler.Event.Result.DENY;
 
 public class SpawnEventHandler {
@@ -19,17 +18,23 @@ public class SpawnEventHandler {
 
     @SubscribeEvent
     public void onCheckSpawn(LivingSpawnEvent.CheckSpawn event) {
-        final Entity entity = event.entity;
+        if (event.entity instanceof EntityLiving) {
+            final EntityLiving entity = (EntityLiving) event.entity;
 
-        if (entity instanceof ISpawnable) {
-            if (!((ISpawnable) entity).canSpawnNow(event.world, event.x, event.y, event.z)) {
-                event.setResult(DENY);
-                if (entity instanceof IWolf)
-                    LOGGER.trace("Denied Spawn for {}", WOLF_REGISTRY.getEntityNameFor((IWolf) entity));
-                return;
-            } else {
-                if (entity instanceof IWolf)
-                    LOGGER.debug("Spawned {}", entity);
+            if (entity instanceof ISpawnable) {
+                if (!((ISpawnable) entity).canSpawnNow(event.world, event.x, event.y, event.z)) {
+                    event.setResult(DENY);
+                    if (entity instanceof IWolf)
+                        LOGGER.trace("Can not spawn now: {}", entity);
+                    return;
+                } else {
+                    if (entity instanceof IWolf) {
+                        if (entity.getCanSpawnHere())
+                            LOGGER.debug("Can spawn here and now: {}", entity);
+                        else
+                            LOGGER.trace("Can spawn now but not here: {}", entity);
+                    }
+                }
             }
         }
     }
