23e6bbd487903928f56217d2068f71a6cae1c085
Lyrael Rayne <darkangael@gmail.com>
2016-05-04 22:11:17 +1000

    Fixed typo in credits.

--- src/main/java/au/lyrael/stacywolves/StacyWolves.java
+++ src/main/java/au/lyrael/stacywolves/StacyWolves.java
@@ -27,11 +27,11 @@ public class StacyWolves {
     public static final String MOD_ID = "stacywolves";
     public static final String MOD_NAME = "Stacy's Wolves Mod";
     public static final String MC_VERSION = "1.7.10";
-    public static final String VERSION  = "1.0.5-" + MC_VERSION;
+    public static final String VERSION  = "1.0.6-" + MC_VERSION;
     public static final String ACCEPTED_MC_VERSIONS = "[" + MC_VERSION + "]";
     public static final String DESC = "Cool Story Bro..... but it Needs Moar Wolves!";
     public static final String URL = "http://mods.curse.com/mc-mods/minecraft/244308-stacys-wolves";
-    public static final String CREDITS = "Concepty by StacyPlays. Code by LyraelRayne and AKTheKnight. Artwork by Jaspanda and Nathan_Oneday of Walschaerts Build Team.";
+    public static final String CREDITS = "Concept by StacyPlays. Code by LyraelRayne and AKTheKnight. Artwork by Jaspanda and Nathan_Oneday of Walschaerts Build Team.";
     public static final String LOGO_PATH = ""; // PUT LOGO PATH HERE!
     public static final String CLIENT_PROXY_CLASS = "au.lyrael.stacywolves.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "au.lyrael.stacywolves.CommonProxy";
