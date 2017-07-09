fae29877c95f38de2cb488018b4263f191cb7ceb
Lyrael Rayne <darkangael@gmail.com>
2017-07-09 12:59:58 +1000

    bugfix: Fixed metadata utility not logging the correct information when registering wolves

--- src/main/java/au/lyrael/stacywolves/utility/MetadataUtility.java
+++ src/main/java/au/lyrael/stacywolves/utility/MetadataUtility.java
@@ -6,6 +6,6 @@ import java.text.MessageFormat;
 
 public class MetadataUtility {
     public static String toString(WolfMetadata metadata) {
-        return MessageFormat.format("Wolf Metadata [name = {1}, primaryColor = {2}, secondaryColor = {3}, number of spawn entries {4}]", metadata.name(), metadata.primaryColour(), metadata.secondaryColour(), metadata.spawns().length);
+        return MessageFormat.format("Wolf Metadata [name = {0}, primaryColor = {1}, secondaryColor = {2}, number of spawn entries {3}]", metadata.name(), metadata.primaryColour(), metadata.secondaryColour(), metadata.spawns().length);
     }
 }
